d96bbeace3758d0526d46e78a9082b876f71e979
sunlover83 <308776759+sunlover83@users.noreply.github.com>
2026-07-26 15:33:44 +0200

    Add apt update installation

--- src/modules/update/update.sh
+++ src/modules/update/update.sh
@@ -46,6 +46,76 @@ check_for_updates() {
     read -rp "Press Enter to continue..."
 }
 
+install_updates() {
+    local -a available_updates=()
+    local confirmation
+
+    detect_distribution
+    draw_module_header "Install Updates"
+
+    if [[ "$PKG_MANAGER" != "apt" ]]; then
+        log_warning "Package manager '${PKG_MANAGER}' is not supported yet."
+        echo
+        read -rp "Press Enter to continue..."
+        return
+    fi
+
+    log_info "Refreshing package information..."
+    echo
+
+    if ! sudo apt-get update; then
+        echo
+        log_error "Package information could not be refreshed."
+        echo
+        read -rp "Press Enter to continue..."
+        return
+    fi
+
+    mapfile -t available_updates < <(
+        apt list --upgradable 2>/dev/null | sed '1d'
+    )
+
+    echo
+
+    if (( ${#available_updates[@]} == 0 )); then
+        log_success "The system is already up to date."
+        echo
+        read -rp "Press Enter to continue..."
+        return
+    fi
+
+    log_warning "${#available_updates[@]} update(s) will be installed."
+    echo
+
+    printf '%s\n' "${available_updates[@]}"
+    echo
+
+    read -rp "Continue with installation? [y/N]: " confirmation
+
+    if [[ ! "$confirmation" =~ ^[Yy]$ ]]; then
+        echo
+        log_info "Installation cancelled."
+        echo
+        read -rp "Press Enter to continue..."
+        return
+    fi
+
+    echo
+    log_info "Installing updates..."
+    echo
+
+    if sudo apt-get upgrade --assume-yes; then
+        echo
+        log_success "Updates installed successfully."
+    else
+        echo
+        log_error "Updates could not be installed completely."
+    fi
+
+    echo
+    read -rp "Press Enter to continue..."
+}
+
 show_update_menu() {
     local choice
 
@@ -67,10 +137,7 @@ show_update_menu() {
                 check_for_updates
                 ;;
             2)
-                echo
-                log_warning "Installing updates is not implemented yet."
-                echo
-                read -rp "Press Enter to continue..."
+                install_updates
                 ;;
             0)
                 return
